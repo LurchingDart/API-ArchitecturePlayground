36ef618009db1eec64b29b638f87c728e092170d
LurchingDart <luizalferez25@gmail.com>
2024-02-07 23:59:23 -0600

    Cors added

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const express = require('express');
-
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 //-----Routes-----//
 const circle = require('./routes/circle');
